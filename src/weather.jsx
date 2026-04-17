0480ebc2660b228ef2a296471f90d8f87ecdb541
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-04-17 20:20:30 +0430

    Changes

--- src/weather.jsx
+++ src/weather.jsx
@@ -80,7 +80,7 @@ export default function Weather(){
                 <h1 className="text-xl">{sunset}</h1>
               </div>
 <div>
-  <h1>hello e</h1>
+  <h1>hello ev</h1>
 
 </div>
               
