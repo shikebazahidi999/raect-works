3fc155bf5d4929778933aca6d9016348c3263436
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-04-18 20:50:46 +0430

    cahnges

--- src/weather.jsx
+++ src/weather.jsx
@@ -80,7 +80,7 @@ export default function Weather(){
                 <h1 className="text-xl">{sunset}</h1>
               </div>
 <div>
-  <h1>hello everyone today i'm going to</h1>
+  <h1>hello everyone today i'm going to t</h1>
 
 </div>
               
