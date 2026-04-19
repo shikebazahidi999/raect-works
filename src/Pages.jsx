07fbbcf6d3e263b104da6e04fc0d6cf6af05bf09
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-04-19 10:26:40 +0430

    changes

--- src/Pages.jsx
+++ src/Pages.jsx
@@ -3,27 +3,30 @@ import Navbar from "./App";
 import { Search } from "lucide-react";
 
 export default function Pages(){
-   const [qimat, setqimat] = useState('')
+   const [qimat, setqimat] = useState("")
     const [value, setvalue] = useState([]);
    const [clickedBtn, setclickedbtn]= useState(false)
     useEffect(()=>{
        async function posts(){
           const Data = await fetch("https://jsonplaceholder.typicode.com/posts");
           const Datas = await Data.json();
-          setvalue(Datas)
+          setvalue(Datas);
+           if(qimat.trim().length> 0){
+        const qimats =setqimat( value.filter((v)=> v.title.includes(qimat)))}
         }
+        
         posts()
     },[]);
-    function ClickButton(){
-        if(qimat.trim().length> 0){
-        const qimats = value.filter((v)=> value.title.includes(value))}
-    }
+  
     if(value.length===0){
         return(
             <div className="h-full w-full flex items-center justify-center ">
             <h1 className=" text-3xl text-center ">Wait For It...</h1>
             </div>
         )
+    };
+      function ClickButton(){
+       
     }
     return(
         <>
@@ -36,7 +39,7 @@ export default function Pages(){
             <div className="grid grid-cols-2 gap-5 p-5 " >
                 {
                     value.map((p)=>(
-                        <div className=" bg-stone-200 gap-4 h-fit w-fit p-2 rounded-2xl hover:shadow-2xl hover:shadow-stone-700 transition-all duration-200  ">
+                        <div key={p.title} className=" bg-stone-200 gap-4 h-fit w-fit p-2 rounded-2xl hover:shadow-2xl hover:shadow-stone-700 transition-all duration-200  ">
                             <h1 className="text-2xl font-bold font-serif bg-linear-90 from-stone-800 via-stone-400 to-stone-800 w-fit text-transparent bg-clip-text">{p.title}:</h1>
                             <p>{p.body}</p>
                         </div>
