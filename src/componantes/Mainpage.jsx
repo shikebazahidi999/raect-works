d89e579e90dbaec0f246d93a8afcc00d926c6881
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-04-20 21:12:30 +0430

    changes

--- src/componantes/Mainpage.jsx
+++ src/componantes/Mainpage.jsx
@@ -1,4 +1,4 @@
-import { useState,createContext } from "react"
+import { createContext, useState, } from "react"
 import Navbar from "../App"
 
 import About from "../about";
@@ -8,23 +8,12 @@ export default function MainPage(){
    
     
      let [value,setvalue] =useState("");
-     function click (){
-        
-        const nameContext = createContext();
-
-     }
+  
+     const nameContext = createContext();
     return(
        
         <>
-       
-        
-        {/* <div className="h1style" >
-            <div className="h1div">
-        <h1>{value}</h1>
-        <button id="btn" onClick={()=> setvalue( value = "Hello world")  }>show Text</button>
-        <button onClick={()=> setvalue(value ="")}>hide text</button>
-        </div>
-        </div> */}
+     
         <Navbar/>
   <nameContext.provideer value={value}>
     <About/>
@@ -32,7 +21,8 @@ export default function MainPage(){
         <div className="h1div flex flex-col gap-4">
             <input className=" border " value={value} onChange={(event)=> setvalue(event.target.value)} type="text" />
             <h1 className=" border border-black p-2 font-serif bg-linear-90 from-green-900 via-green-300 to-green-900 text-transparent bg-clip-text ">{value}</h1>
-            <button className="border font-bold hover:bg-gray-400 hover:text-white transition-all duration-150 hover:rounded-md" onClick={click}>hide text</button>
+            <button className="border font-bold hover:bg-gray-400 hover:text-white transition-all duration-150 hover:rounded-md"
+             >hide text</button>
         </div>
         </>
     )
