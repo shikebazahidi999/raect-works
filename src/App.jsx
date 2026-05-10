455c07dc9db6068396cc747922a4d8c8067c6cb7
shikebazahidi999 <shikebazahedi41@gmail.com>
2026-05-10 21:03:34 +0430

    the usereaducer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef } from "react";
+import { createContext, useContext, useReducer, useRef } from "react";
 import {  useEffect} from "react";
 import { Link,  } from "react-router-dom";
 import BgChanger from "./bgChanger";
@@ -52,13 +52,24 @@ export default function Navbar(){
 </ul> </div> </>
   );
 }
+
+
 import {  useState, } from "react";
 import About from "./about";
 import Radiucer from "./useraduce";
+import Textcomponent from "./text";
  export const NameContext = createContext();
+export const usecontexts = createContext();
 export  function MainPage(){
 
   let [value,setvalue] =useState();
+  let text = "hello";
+ function textfunction(values,setvalues){
+     return(
+    values
+     )
+ }
+ let  [values, setvalues] =useReducer(textfunction,text)
   let getvalue = useRef();
 
   function click(){
@@ -67,9 +78,13 @@ setvalue(getvalue.current.value);
    return(
         <>
       <Navbar/>
-        <NameContext.Provider value={value}>
+        <usecontexts.Provider value={textfunction}>
+         <Textcomponent/>
+          </usecontexts.Provider>
+        <NameContext.Provider value={{value:values,setvalues:setvalues}}>
           <About/>
           </NameContext.Provider>
+        
    <div className="flex flex-row items-center justify-center p-20 h-fit bg-linear-90 from-red-200 via-sky-200 to-lime-200 ">        
 {/* <div className="h1div flex flex-col gap-4">
             <input ref={getvalue} className=" border " value={value} onChange={(event)=> setvalue(event.target.value)} type="text" />
